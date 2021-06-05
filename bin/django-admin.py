f9f7e2f41d2554a00a5ef51fb08bf618cef95923
Dil Madale <dilmadale@Dils-MacBook-Air.local>
2021-06-05 19:28:25 +0530

    First commit

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/dilmadale/get-started-python2/Django/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ src/pages/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home_view(request, *args, **kwargs):
+    print(request.user)
+    return HttpResponse("<h1> Hello World</h1>")
+
+def contact_view(request, *args, **kwargs):
+    return HttpResponse("<h1> Contact page</h1>")
+
+def about_view(request, *args, **kwargs):
+    return HttpResponse("<h1> About page</h1>")
+
+def social_view(request, *args, **kwargs):
+    return HttpResponse("<h1> Social page</h1>")
