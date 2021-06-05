21a2c83b60d0889e8739cfae306e0409de888f40
Dil Madale <dilmadale@Dils-MacBook-Air.local>
2021-06-05 19:46:27 +0530

    Editing Setting

--- src/trydjango/settings.py
+++ src/trydjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')&*ru4p+bm3$=b#9fc9xj4*%d#qbi*u_zwpn*!!k-zd&y3i8h!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','localhost','0.0.0.0','192.168.1.7']
 
 
 # Application definition
